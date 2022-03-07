0bf9c18be184ea4451304e02f90d69b3edd6c24c
LorenzoRottigni <lorenzo@rottigni.net>
2022-03-07 18:47:39 +0100

    radar icons function

--- components/Radar.vue
+++ components/Radar.vue
@@ -1,24 +1,68 @@
 <template lang="pug">
-#radar.border.border-primary
-    .radar-quadrant(
+#radar.border.border-primary.d-flex.flex-wrap
+    .radar-quadrant.d-flex.align-items-center.justify-content-center(
         :key="'radar-quadrant-' + index"
-        v-for="(route, index) in vueRoutes"
+        v-for="(route, index) in getVueRoutes"
     )
-        h1.text-primary {{ route.name }}
+        h6.text-primary {{ route.name }}
 </template>
 
 <script lang="ts" setup>
-//const vueRouter = useRouter()
-//const activeRoute = useRoute()
-//const activeRouteName = activeRoute.name
-//const activeRoutePath = activeRoute.path
-//const vueRoutes = vueRouter.options.routes
 
-const getVueRoute = computed(() => useRouter()).value
+const getIconByRouteName = (routeName) => {
+    switch(routeName){
+        case 'index':
+            return 'fas fa-home fa-lg'
+        case 'blog':
+            return 'fas fa-th-list fa-lg'
+        case 'app':
+            return 'fas fa-home fa-lg'
+        case 'contacts':
+            return 'far fa-id-badge fa-lg'
+        case 'cv':
+            return 'far fa-file-alt fa-lg'
+        case 'github':
+            return 'fab fa-github fa-lg'
+        case 'vimeo':
+            return 'fab fa-vimeo-v fa-lg'
+        case 'skills':
+            return 'fas fa-cloud fa-lg'
+        case 'portfolio':
+            return 'fas fa-code fa-lg'
+        default:
+            return 'fas fa-error fa-lg'
+    }
+}
+
+/**
+ * @description get VueRouter istance
+ * @returns {object} istance of VueRouter
+ */
+const getVueRouter = computed(() => useRouter()).value
+/**
+ * @description get current VueRouter route
+ * @returns {object} {name},{path},{location}
+ */
 const getActiveRoute = computed(() => useRoute()).value
+/**
+ * @description get current VueRouter name
+ * @returns {string}
+ */
 const getActiveRouteName = computed(() => getActiveRoute.name).value
+/**
+ * @description get current VueRouter route path
+ * @returns {string}
+ */
 const getActiveRoutePath = computed(() => getActiveRoute.path).value
-const getVueRoutes = computed(() => getVueRoute.options.routes).value
+/**
+ * @description get VueRouter routes unordered list
+ * @returns {array}
+ */
+const getVueRoutes = computed(() => getVueRouter.options.routes).value
+/**
+ * @description get VueRouter routes ordered list
+ * @returns {array}
+ */
 const getRadarVueRoutes = computed(() => {
     const unorderedList = getVueRoutes
     const centralItem = unorderedList[4]
@@ -42,8 +86,14 @@ console.log(getRadarVueRoutes)
 </script>
 
 <style lang="sass" scoped>
+$vh-base: 33vh
 #radar
     position: absolute
     top: 10px
     left: 10px
+    width: $vh-base
+    height: $vh-base
+    .radar-quadrant
+        width: calc(100% / 3)
+        height: calc(100 / 3)
 </style>
